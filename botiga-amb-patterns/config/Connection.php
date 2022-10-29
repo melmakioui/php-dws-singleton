789a308fe27dd07004019bf7924c5c219933a19f
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-10-29 14:15:03 +0200

    Canvi de configuracio instancia PDO

--- botiga-amb-patterns/config/Connection.php
+++ botiga-amb-patterns/config/Connection.php
@@ -1,12 +1,19 @@
 <?php
+require_once "config.php";
+var_dump($db);
 
 class Connection {
 
     public static $connection;
+    private $conn;
+    private $host = $db["host"];
+    private $username = $db["user"];
+    private $password = $db["password"];
+
 
     private function __construct()
     {
-
+        $this->conn = new PDO($this->host,$this->username,$this->password);
     }
 
     public static function getInstance () {
--- botiga-amb-patterns/config/config.php
+++ botiga-amb-patterns/config/config.php
@@ -1,6 +1,7 @@
 <?php
 
 $db = array (
+    "host"=> "mysql:host=127.0.0.1;dbname=shop",
     "user"=>"root",
     "password"=>"root"
 );
