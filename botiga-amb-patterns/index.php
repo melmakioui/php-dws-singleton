68f8a2f6766b03301a0a4894aeb099f2964d656e
Mohamed <91630250+melmakioui@users.noreply.github.com>
2022-10-29 23:06:43 +0200

    Front controller operatiu, finalitzat

--- botiga-amb-patterns/index.php
+++ botiga-amb-patterns/index.php
@@ -1,27 +1,33 @@
-<?
+<?php
 require_once './validation/RegisterProduct.php';
 
 
-    if( !isset($_POST['name']) && 
-        !isset($_POST['description']) && 
-        !isset($_POST['price']) &&
-        !isset($_FILES['file']))
-           header("Location: ./views/List.php?err=true");
+function isValidInput(){
+return
+    isset($_POST["name"]) &&
+    isset($_POST["description"]) &&
+    isset($_POST["price"]) &&
+    isset($_FILES["file"]);
+}
 
+if(!isValidInput())
+    header("Location: /views/List.php");
+else {
+    $register = new RegisterProduct();
 
-
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+        
+    $register->insertProduct($_POST);
+    $register->uploadFile($_FILES);
+    unset($_POST);
+    unset($_FILES);
+        
+    header("Location: /views/List.php");
+   
+}           
   
-$register = new RegisterProduct();
-
-$name = $_POST['name'];
-$description = $_POST['description'];
-$price = $_POST['price'];
-
-
-$register->insertProduct($_POST);
-$register->uploadFile($_FILES);
-
-require_once './views/List.php';
 
 
 
